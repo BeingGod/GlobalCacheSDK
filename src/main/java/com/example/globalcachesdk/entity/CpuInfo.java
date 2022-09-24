c9a7e2453860a1436ace8432a27fe94fd53eb2d9
BeingGod <beinggod@foxmail.com>
2022-09-24 11:43:04 +0800

    优化entity类

--- src/main/java/com/example/globalcachesdk/entity/CpuInfo.java
+++ src/main/java/com/example/globalcachesdk/entity/CpuInfo.java
@@ -1,15 +1,13 @@
 package com.example.globalcachesdk.entity;
 
-import lombok.Data;
-
 import java.util.ArrayList;
 
 /**
  * 节点CPU信息
- * @author RP Cai
+ * @author 蔡润培
  */
-@Data
-public class CpuInfo {
+public class CpuInfo extends AbstractEntity {
+
     /**
      * 节点CPU整体占用率
     */
@@ -19,4 +17,32 @@ public class CpuInfo {
      * 节点逻辑核心占用率 单位 百分比
      */
     private ArrayList<Double> coreUsage;
+
+    /**
+     * @return CPU核心整体占用率，以百分比为单位
+     */
+    public double getTotalUsage() {
+        return totalUsage;
+    }
+
+    /**
+     * @param totalUsage CPU核心整体占用率，以百分比为单位
+     */
+    public void setTotalUsage(double totalUsage) {
+        this.totalUsage = totalUsage;
+    }
+
+    /**
+     * @return CPU所有逻辑核心占用率，以百分比为单位
+     */
+    public ArrayList<Double> getCoreUsage() {
+        return coreUsage;
+    }
+
+    /**
+     * @param coreUsage CPU所有逻辑核心占用率，以百分比为单位
+     */
+    public void setCoreUsage(ArrayList<Double> coreUsage) {
+        this.coreUsage = coreUsage;
+    }
 }
