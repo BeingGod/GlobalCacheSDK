cf4387c96fe882782a9650cf101955a60e15f18b
beinggod <beinggod@foxmail.com>
2023-03-20 20:05:25 +0800

    [fix] fix query disk info

--- src/main/java/com/hw/globalcachesdk/executorImpl/QueryDiskInfo.java
+++ src/main/java/com/hw/globalcachesdk/executorImpl/QueryDiskInfo.java
@@ -19,9 +19,6 @@ import java.util.regex.Pattern;
 @Script(path = "/home/GlobalCacheScriptsNew/data/disk/disk.sh")
 public class QueryDiskInfo extends AbstractCommandExecutorSync {
 
-    private static final Pattern DISK_INFO_PATTERN = Pattern.compile("[a-z].*");
-    private static final Pattern DISK_ROTA_PATTERN = Pattern.compile("^sd.");
-    private static final Pattern DISK_NVME_PATTERN = Pattern.compile("^nvme.");
     public QueryDiskInfo() {
         super(QueryDiskInfo.class);
     }
@@ -30,18 +27,22 @@ public class QueryDiskInfo extends AbstractCommandExecutorSync {
     public AbstractEntity parseOf(String returnValue) throws ReturnValueParseException {
         DiskInfo diskInfo = new DiskInfo();
 
-        Matcher matcher = DISK_INFO_PATTERN.matcher(returnValue);
         ArrayList<DiskInfo.Disk> diskList = new ArrayList<>();
-        while (matcher.find()) {
+        //返回值按行切分
+        String[] diskListSring = returnValue.split("\n");
+        for (String diskInfoString:diskListSring) {
+            //行内按空格（包含连续空格）切分0为名称1为类型
+            String[] diskInfoListSring = diskInfoString.replaceAll("\\s{2,}", " ").split(" ");
             DiskInfo.Disk disk = new DiskInfo.Disk();
-            disk.setName(matcher.group(0));
-            if (DISK_ROTA_PATTERN.matcher(matcher.group(0)).find()) {
+            disk.setName(diskInfoListSring[0]);
+            if("0".equals(diskInfoListSring[1])){
                 disk.setType(DiskInfo.DiskType.ROTA);
-            } else if (DISK_NVME_PATTERN.matcher(matcher.group(0)).find()) {
+            } else if ("1".equals(diskInfoListSring[1])) {
                 disk.setType(DiskInfo.DiskType.NVME);
             }
             diskList.add(disk);
         }
+
         diskInfo.setDisksList(diskList);
 
         return diskInfo;
