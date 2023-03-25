e6152ad6ad160163ec8c7d512402bbaf09a45310
msprc <1459190927@qq.com>
2023-03-25 15:55:11 +0800

    QueryUptimeInfo.java新脚本适配

--- src/main/java/com/hw/globalcachesdk/executorImpl/QueryUptimeInfo.java
+++ src/main/java/com/hw/globalcachesdk/executorImpl/QueryUptimeInfo.java
@@ -27,20 +27,30 @@ public class QueryUptimeInfo extends AbstractCommandExecutorSync {
         } else {
             int date = 0, hour = 0, min = 0;
             returnValue = returnValue.substring(0, returnValue.length() - 1);
+            //returnValue="6days , 55 min,";
+            //判断是否有day的输入
             int dateIndex = returnValue.indexOf("d");
             if (dateIndex != -1) {
+                //获取日期
                 date = Integer.parseInt(returnValue.substring(0, dateIndex).trim());
+                //处理字符串，去掉日期相关内容
+                returnValue=returnValue.substring(returnValue.indexOf(",")+1);
             }
+            //判断显示格式，带不带小时
             int hourIndex = returnValue.indexOf(":");
-            if (hourIndex == 1){
-                hour = Integer.parseInt(returnValue.substring(0, 1).trim());
-            } else {
-                hour = Integer.parseInt(returnValue.substring(hourIndex - 2, hourIndex).trim());
-            }
-            if (returnValue.substring(hourIndex + 1).contains(",")){
+            if(hourIndex!=-1){
+                //带小时，显示格式：“  5:03,“
+                if (hourIndex == 1){
+                    hour = Integer.parseInt(returnValue.substring(0, 1).trim());
+                } else {
+                    hour = Integer.parseInt(returnValue.substring(hourIndex - 2, hourIndex).trim());
+                }
+                //去掉行尾的","
                 min = Integer.parseInt(returnValue.substring(hourIndex + 1).replaceAll(",","").trim());
-            } else{
-                min = Integer.parseInt(returnValue.substring(hourIndex + 1).trim());
+            }else{
+                //不带小时，显示格式：“  55min,”
+                int minIndex=returnValue.indexOf("m");
+                min=Integer.parseInt( returnValue.substring(0,minIndex).trim());
             }
             uptimeInfo.setUptime((long) date * 24 * 60 + hour * 60L +min);
         }
