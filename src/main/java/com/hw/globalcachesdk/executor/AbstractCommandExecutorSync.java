e6690e26b03ae1f0f7fc012cba570fec9c7baf33
BeingGod <beinggod@foxmail.com>
2023-07-01 21:20:58 +0800

    fix: return value bug

--- src/main/java/com/hw/globalcachesdk/executor/AbstractCommandExecutorSync.java
+++ src/main/java/com/hw/globalcachesdk/executor/AbstractCommandExecutorSync.java
@@ -156,7 +156,7 @@ public abstract class AbstractCommandExecutorSync extends AbstractCommandExecuto
 
             String line = "";
             while ((line = input.readLine()) != null) {
-                returnValue.append(line);
+                returnValue.append(line).append("\n");
             }
             input.close();
 
